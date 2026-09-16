18ccd0e8ad2698b398b0aeeaa672509961c1337d
lucabose80-cmd <lucabose80@gmail.com>
2026-09-16 16:47:30 +0200

    fix: merge fcm into next-pwa service worker

--- src/lib/fcm.ts
+++ src/lib/fcm.ts
@@ -12,7 +12,11 @@ export const requestForToken = async (userId: string) => {
       
       const permission = await Notification.requestPermission();
       if (permission === "granted") {
-        const currentToken = await getToken(messaging, { vapidKey: VAPID_KEY });
+        const registration = await navigator.serviceWorker.ready;
+        const currentToken = await getToken(messaging, { 
+          vapidKey: VAPID_KEY,
+          serviceWorkerRegistration: registration 
+        });
         if (currentToken) {
           // Save token to user profile
           if (db && userId) {
